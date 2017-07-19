8b865ab8d6ce7329b597d8633519baed7a8cc912
dbennet <david.g.bennett10@gmail.com>
2017-07-19 07:11:04 -0700

    adding db entries to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,10 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name: 'Star Wars' }, { name:a 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+	staging_city = City.create(:name=>'Baltimore')
+	staging_state = State.create(:name=>'Maryland')
+
+
